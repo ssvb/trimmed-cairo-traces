ef25829aacd055c60903fc0e7dc16e44cb95d24c
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2013-01-31 01:34:30 +0200

    Tweaks for bench.sh to make it more user friendly
    Now it suggests the list of backends to test if none is provided.
    Also it generates both human readable and raw benchark results.

--- bench.sh
+++ bench.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-export CAIRO_TEST_TARGET=image
+if [ x$CAIRO_TEST_TARGET == "x" ]
+then
+    echo "CAIRO_TEST_TARGET enviromnent variable is not set, the possible options are:"
+    echo
+    echo "   export CAIRO_TEST_TARGET=image   (software rendering, 32 bits per pixel)"
+    echo "   export CAIRO_TEST_TARGET=image16 (software rendering, 16 bits per pixel)"
+    echo "   export CAIRO_TEST_TARGET=xlib    (use the 2D accelerated driver from X server)"
+    echo
+    echo "In the case if you are running in a ssh session and still want to benchmark"
+    echo "the 2D acceleration provided by the X server, sometimes you may need to also:"
+    echo
+    echo "   export DISPLAY=:0"
+    echo
+    echo "Please set the needed environment variables as explained above and try again."
+    exit 1
+fi
 
 export PREFIX=`pwd`/tmp
 export LD_LIBRARY_PATH=$PREFIX/pixman/lib:$PREFIX/cairo/lib
@@ -12,4 +27,4 @@ fi
 
 # run the benchmarks
 
-cairo/perf/.libs/cairo-perf-trace benchmark
+cairo/perf/.libs/cairo-perf-trace benchmark -r -v -i8 > results-raw.txt
